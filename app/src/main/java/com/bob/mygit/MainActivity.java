ceb31f5272fab9fafafa6f0be6b0650331812e2a
linzx@ifeng.com <xiang123-123>
2020-02-22 17:06:52 +0800

    dev1_my222_222

--- app/src/main/java/com/bob/mygit/MainActivity.java
+++ app/src/main/java/com/bob/mygit/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_feature_2;
     private Button btn_feature_4;
     private Button btn_feature_newbranch1;
+    private Button btn_feature_newbranch2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         btn_feature_4 = (Button) findViewById(R.id.btn_feature_4);
         btn_feature_4 = (Button) findViewById(R.id.btn_feature_4);
         btn_feature_newbranch1 = (Button) findViewById(R.id.btn_feature_4);
+        btn_feature_newbranch2 = (Button) findViewById(R.id.btn_feature_4);
     }
 
     /**
