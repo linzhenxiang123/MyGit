0b6ca68cabdf973729fbf62f2930e77a6ac55c4e
linzx@ifeng.com <xiang123-123>
2020-02-22 23:00:07 +0800

    测试rebase的合并节点
    develop_rebase2
    
    develop_rebase33

--- app/src/main/java/com/bob/mygit/TestActivity.java
+++ app/src/main/java/com/bob/mygit/TestActivity.java
@@ -8,6 +8,9 @@ public class TestActivity extends AppCompatActivity {
 
     private int hello = 0;
     private int hello22 = 22;
+    private boolean aaa = true;
+    private boolean bbb = false;
+    private boolean ccc = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
